1cd8d758ab7218b1617bf55f0dfbaa7e5d66b1c2
antonboec1994 <103438295+antonboec1994@users.noreply.github.com>
2025-08-11 13:34:05 +0300

    deploy

--- deploy.js
+++ deploy.js
@@ -2,7 +2,7 @@ import http from 'http';
 import crypto from 'crypto';
 import { exec } from 'child_process';
 
-//Тест проверка!
+//Тест проверка!!
 const PORT = 3004;
 const SECRET_TOKEN = 'supersecrettoken123';
 const REPO_DIR = '/root/market';
