fe3a90aee2b88f28057b2ecd2b7a3c08c9d4f049
antonboec1994 <103438295+antonboec1994@users.noreply.github.com>
2025-09-15 09:49:12 +0300

    fixed 15/09

--- frontend/src/components/Content/Content.tsx
+++ frontend/src/components/Content/Content.tsx
@@ -1,27 +1,25 @@
-// import React, { lazy } from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import styles from './Content.module.scss';
 import background from '../../assets/images/content-background.jpg';
 
-// УДАЛИТЬ: const HomePage = lazy(() => import(...));
-// ЗАМЕНИТЬ на обычный импорт:
-import HomePage from '../../pages/HomePage';
-import AboutPage from '../../pages/AboutPage';
-import ContactsPage from '../../pages/ContactsPage';
-import DeliveryPage from '../../pages/DeliveryPage';
-import PartnershipPage from '../../pages/PartnershipPage';
-import PaymentPage from '../../pages/PaymentPage';
-import ProductPage from '../../pages/ProductPage';
-import CatalogPage from '../../pages/CatalogPage';
-import ShowroomPage from '../../pages/ShowroomPage';
-import CartPage from '../../pages/CartPage';
-import ProfilePage from '../../pages/ProfilePage';
-import ErrorPage from '../../pages/ErrorPage';
+const HomePage = lazy(() => import('../../pages/HomePage'));
+const AboutPage = lazy(() => import('../../pages/AboutPage'));
+const ContactsPage = lazy(() => import('../../pages/ContactsPage'));
+const DeliveryPage = lazy(() => import('../../pages/DeliveryPage'));
+const PartnershipPage = lazy(() => import('../../pages/PartnershipPage'));
+const PaymentPage = lazy(() => import('../../pages/PaymentPage'));
+const ProductPage = lazy(() => import('../../pages/ProductPage'));
+const CatalogPage = lazy(() => import('../../pages/CatalogPage'));
+const ShowroomPage = lazy(() => import('../../pages/ShowroomPage'));
+const CartPage = lazy(() => import('../../pages/CartPage'));
+const ProfilePage = lazy(() => import('../../pages/ProfilePage'));
+const ErrorPage = lazy(() => import('../../pages/ErrorPage'));
 
 const Content: React.FC = () => {
 	const location = useLocation();
 	const urlPathProductPage = location.pathname.includes('product');
-	// const fallbackLoading = <div>Идёт загрузка страницы...</div>;
+	const fallbackLoading = <div>Идёт загрузка страницы...</div>;
 
 	const myRoutes = [
 		{
@@ -83,21 +81,13 @@ const Content: React.FC = () => {
 			style={{ backgroundImage: 'url(' + background + ')' }}
 		>
 			<div className='container'>
-				<Routes>
-					{myRoutes.map((item, index) => (
-						//Из-за проблем с чанками временно заккоментировано
-						// <Route
-						// 	path={item.path}
-						// 	key={index}
-						// 	element={
-						// 		<React.Suspense fallback={fallbackLoading}>
-						// 			{item.element}
-						// 		</React.Suspense>
-						// 	}
-						// ></Route>
-						<Route key={index} path={item.path} element={item.element} />
-					))}
-				</Routes>
+				<Suspense fallback={fallbackLoading}>
+					<Routes>
+						{myRoutes.map((item, index) => (
+							<Route key={index} path={item.path} element={item.element} />
+						))}
+					</Routes>
+				</Suspense>
 			</div>
 		</div>
 	);
