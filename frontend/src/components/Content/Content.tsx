a3c58ad37d6bef45e5148199d9677e94f260d2c4
antonboec1994 <103438295+antonboec1994@users.noreply.github.com>
2025-09-17 14:04:51 +0300

    fix bugs

--- frontend/src/components/Content/Content.tsx
+++ frontend/src/components/Content/Content.tsx
@@ -1,20 +1,32 @@
-import React, { lazy, Suspense } from 'react';
+import React, { Suspense } from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import styles from './Content.module.scss';
 import background from '../../assets/images/content-background.jpg';
+import HomePage from '@/pages/HomePage';
+import AboutPage from '@/pages/AboutPage';
+import ContactsPage from '@/pages/ContactsPage';
+import DeliveryPage from '@/pages/DeliveryPage';
+import PartnershipPage from '@/pages/PartnershipPage';
+import PaymentPage from '@/pages/PaymentPage';
+import ProductPage from '@/pages/ProductPage';
+import CatalogPage from '@/pages/CatalogPage';
+import ShowroomPage from '@/pages/ShowroomPage';
+import CartPage from '@/pages/CartPage';
+import ProfilePage from '@/pages/ProfilePage';
+import ErrorPage from '@/pages/ErrorPage';
 
-const HomePage = lazy(() => import('../../pages/HomePage'));
-const AboutPage = lazy(() => import('../../pages/AboutPage'));
-const ContactsPage = lazy(() => import('../../pages/ContactsPage'));
-const DeliveryPage = lazy(() => import('../../pages/DeliveryPage'));
-const PartnershipPage = lazy(() => import('../../pages/PartnershipPage'));
-const PaymentPage = lazy(() => import('../../pages/PaymentPage'));
-const ProductPage = lazy(() => import('../../pages/ProductPage'));
-const CatalogPage = lazy(() => import('../../pages/CatalogPage'));
-const ShowroomPage = lazy(() => import('../../pages/ShowroomPage'));
-const CartPage = lazy(() => import('../../pages/CartPage'));
-const ProfilePage = lazy(() => import('../../pages/ProfilePage'));
-const ErrorPage = lazy(() => import('../../pages/ErrorPage'));
+// const HomePage = lazy(() => import('../../pages/HomePage'));
+// const AboutPage = lazy(() => import('../../pages/AboutPage'));
+// const ContactsPage = lazy(() => import('../../pages/ContactsPage'));
+// const DeliveryPage = lazy(() => import('../../pages/DeliveryPage'));
+// const PartnershipPage = lazy(() => import('../../pages/PartnershipPage'));
+// const PaymentPage = lazy(() => import('../../pages/PaymentPage'));
+// const ProductPage = lazy(() => import('../../pages/ProductPage'));
+// const CatalogPage = lazy(() => import('../../pages/CatalogPage'));
+// const ShowroomPage = lazy(() => import('../../pages/ShowroomPage'));
+// const CartPage = lazy(() => import('../../pages/CartPage'));
+// const ProfilePage = lazy(() => import('../../pages/ProfilePage'));
+// const ErrorPage = lazy(() => import('../../pages/ErrorPage'));
 
 const Content: React.FC = () => {
 	const location = useLocation();
