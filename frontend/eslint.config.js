ca71428058120b87910d81453f5dc093d6553b21
antonboec1994 <103438295+antonboec1994@users.noreply.github.com>
2025-12-26 11:56:42 +0300

    Установлены зависимости для работы jest

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
+import jestPlugin from 'eslint-plugin-jest';
 
 export default tseslint.config(
 	{ ignores: ['dist'] },
@@ -11,7 +12,8 @@ export default tseslint.config(
 			js.configs.recommended,
 			...tseslint.configs.recommended,
 			'plugin:import/recommended', // Добавляем плагин для работы с импортами
-			'plugin:import/typescript', // Поддержка TypeScript для импортов
+			'plugin:import/typescript', // Поддержка TypeScript для импортов,
+			'plugin:jest/recommended',
 		],
 		files: ['**/*.{ts,tsx}'],
 		languageOptions: {
@@ -22,6 +24,7 @@ export default tseslint.config(
 			'react-hooks': reactHooks,
 			'react-refresh': reactRefresh,
 			import: require('eslint-plugin-import'), // Добавляем плагин import
+			jest: jestPlugin,
 		},
 		settings: {
 			'import/resolver': {
@@ -38,6 +41,7 @@ export default tseslint.config(
 			],
 			'import/no-unresolved': 'error', // Проверка неразрешенных импортов
 			'import/extensions': ['error', 'never'], // Запрет расширений (.ts, .tsx) в импортах
+			'jest/no-focused-tests': 'off',
 		},
 	}
 );
